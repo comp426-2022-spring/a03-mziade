88ca942e8fe9d4dee57f2f549426917f2a102511
Mia Ziade <mziade@live.unc.edu>
2022-02-28 20:56:31 -0500

    base

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 // Require Express.js
 const express = require('express')
 const app = express()
-
-var port = 5000
+const args = require('minimist')(process.argv.slice(2))
+args['port']
+const call = args.call
+const port = args.port || process.env.PORT || 5000
 
 // Start an app server
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
+});
+
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+	res.statusCode = 200;
+	// Respond with status message "OK"
+		res.statusMessage = 'OK';
+		res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+		res.end(res.statusCode+ ' ' +res.statusMessage)
 });
 
 // Default response for any other request
@@ -14,11 +25,10 @@ app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
 });
 
-app.get('/app/', (req, res) => {
-    // Respond with status 200
-        res.statusCode = 200;
-    // Respond with status message "OK"
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
+app.get('/app/flips/:number', (req, res) => {
+	const flips = manyflips(req.params.number)
+	//Other
+	//expressions
+	//go
+	//here
 });
